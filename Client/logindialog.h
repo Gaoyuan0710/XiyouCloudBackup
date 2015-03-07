bd4dfd6f82cf7cf88d40f880289232c42a8b6528
gitliuyuhan <838532811@qq.com>
2015-03-07 14:51:59 +0800

    logindialog.h

---
+++ Client/logindialog.h
@@ -0,0 +1,32 @@
+//登陆界面
+
+#ifndef LOGINDIALOG_H
+#define LOGINDIALOG_H
+
+#include <QDialog>
+
+
+namespace Ui {
+class LoginDialog;
+}
+
+class LoginDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit LoginDialog(QWidget *parent = 0);
+    ~LoginDialog();
+
+private slots:
+void loginButton_clicked();
+
+void replyButton_clicked();
+
+void toolButton_clicked();
+
+private:
+    Ui::LoginDialog *ui;
+};
+
+#endif // LOGINDIALOG_H
